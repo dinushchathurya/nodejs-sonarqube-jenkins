31c783ff5673801a2896e8a25ddeb05aa0c10463
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-12 20:28:52 +0530

    feat: Chanhe index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Jenkins Server");
+  res.send("Hello Jenkins");
 });
 
 const server = app.listen(3000, () => {
